e1e0704bdf25d869463de04950d75b8c9bd08768
Konstantin <johnysalvesen@gmail.com>
2024-01-15 20:21:06 +0300

    Updated ALOOWED_HOSTS;

--- tezos_game_back/settings.py
+++ tezos_game_back/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '45.66.248.137']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '192.168.1.74', '45.66.248.137']
 
 
 # Application definition
