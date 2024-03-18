a9974caeae6cdde0e8e06d5287f0edd2e6b2bbaf
Konst <johnysalvesen@gmail.com>
2024-03-18 20:14:55 +0300

    Added DisableCSRFMiddleware;

--- tezos_game_back/settings.py
+++ tezos_game_back/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
+    'api.middleware.DisableCSRFMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
